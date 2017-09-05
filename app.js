7ccfa5f78abebcea29cf2c4eba3e04cf5c34fa80
edumode <cesarvasz@gmail.com>
2017-09-04 18:51:35 -0600

    Routes

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/dist'));
 
 // respond with "hello world" when a GET request is made to the homepage
-app.use('/', user);
-app.use('/', tools);
-app.use('/', releases);
-app.use('/', guideline);
-app.use('/', today);
+app.use('api/', user);
+app.use('api/', tools);
+app.use('api/', releases);
+app.use('api/', guideline);
+app.use('api/', today);
 
 
 module.exports = app;
